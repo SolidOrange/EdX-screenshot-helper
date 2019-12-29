6a2ee4ba32a68de1781088b9f8dffd3af7ea5b2a
SolidOrange <SolidOrange@users.noreply.github.com>
2019-12-29 18:24:10 -0500

    init

--- remove_element.js
+++ remove_element.js
@@ -1,14 +1,8 @@
-let target_node = document.getElementById("seq_content");
-const config = {childList: true, characterData: true, subtree: true};
-
-const callback = function() {
-    let play_buttons = document.getElementsByClassName("btn-play fa fa-youtube-play fa-2x");
-    if (play_buttons.length > 0){
-        play_buttons[0].remove();
-    }   
+const remove_button = function() {
+    play_button = document.querySelector(".btn-play");
+    if (play_button != null) {
+        play_button.remove();
+    }    
 }
-
-const observer = new MutationObserver(callback);
-observer.observe(target_node, config);
-
-
+const observer = new MutationObserver(remove_button);
+observer.observe(document.getElementById("seq_content"), {childList: true, characterData: true, subtree: true});
